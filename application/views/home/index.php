1ab18878f8d7ab9e9e6f7b3a2c799479b9df1233
Vallabh Kansagara <vrkansgara@gmail.com>
2015-09-20 20:36:51 +0530

    Home controller added view and controller.

---
+++ application/views/home/index.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<h1>CodeIgniter Skeleton Application</h1>
+</body>
+</html>
